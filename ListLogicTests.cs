168964dafbcd5fad77f3cb5b7fc1cc87073560d6
travischapman999 <travischapman999@gmail.com>
2026-04-22 14:38:15 -0600

    Add files via upload

---
+++ ListLogicTests.cs
@@ -0,0 +1,34 @@
+﻿using PlayerStuff;
+using static StaticStuff.MiscellaneousStatic;
+
+namespace FinalProjectCS1410.Tests
+{
+	public class ListLogicTests
+	{
+		[Fact]
+		public void Move_Existing_Player_to_Front_Of_List()
+		{
+			List<Player> players = new List<Player>
+			{
+				new Player("Bob"),
+				new Player("Travis"),
+				new Player("James")
+			};
+			Player player = players.First(p => p.Name == "Travis");
+			MovePlayerToFrontOfList(players, player);
+			Assert.Equal("Travis", players[0].Name);
+		}
+		[Fact]
+		public void Add_New_Player_To_Front_Of_List()
+		{
+			List<Player> players = new List<Player>
+			{
+				new Player("Travis"),
+				new Player("Bob"),
+				new Player("James")
+			};
+			players.Insert(0, new Player("John"));
+			Assert.Equal("John", players[0].Name);
+		}
+	}
+}
---
+++ PlayerTests.cs
@@ -0,0 +1,41 @@
+﻿using PlayerStuff;
+using static StaticStuff.MiscellaneousStatic;
+namespace FinalProjectCS1410.Tests
+{
+	public class PlayerTests
+	{
+		[Fact]
+		public void Return_Player_If_Found()
+		{
+			List<Player> players = new List<Player>
+			{
+				new Player("Travis"),
+				new Player("Bob"),
+				new Player("James")
+			};
+			Player? result = FindPlayerInList(players, "travis");
+
+			Assert.NotNull(result);
+		}
+		[Fact]
+		public void Return_Null_If_Not_Found()
+		{
+			List<Player> players = new List<Player>
+			{
+				new Player("Travis"),
+				new Player("Bob"),
+				new Player("James")
+			};
+			Player? result = FindPlayerInList(players, "karen");
+
+			Assert.Null(result);
+		}
+		[Fact]
+		public void Test_If_Letter_Casing_Correct()
+		{
+			string inputName = "JoHN";
+			string actualName = FixNameCasing(inputName);
+			Assert.Equal("John", actualName);
+		}
+	}
+}
